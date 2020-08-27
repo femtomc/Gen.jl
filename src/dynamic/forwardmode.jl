c986aab62e46a45ccf3adf29e2d36c990e5ee649
McCoy R. Becker <mccoybecker@gmail.com>
2020-08-27 09:56:22 -0400

    Determine if get_submap returns raw value addr denotes a choice.

--- src/dynamic/forwardmode.jl
+++ src/dynamic/forwardmode.jl
@@ -1,25 +1,56 @@
 mutable struct GFForwardModeState{T <: Tuple,
                                   D}
     target::T
-    map::ChoiceMap
+    tr::Trace
     weight::D
     visited::AddressVisitor
     params::Dict{Symbol, Any}
 end
 
-function GFForwardModeState(addr, tr, weight)
-    ForwardModeContext(addr, tr, weight, Visitor(), Empty())
+function GFForwardModeState(key, tr, weight)
+    ForwardModeContext(key, 
+                       tr, 
+                       weight, 
+                       Visitor(), 
+                       Dict{Symbol, Any}())
 end
 
-function GFForwardModeState(addr, params, tr, weight)
-    ForwardModeContext(addr, tr, weight, AddressVisitor(), params)
+function GFForwardModeState(key, params, tr, weight)
+    ForwardModeContext(key, 
+                       tr, 
+                       weight, 
+                       AddressVisitor(), 
+                       params)
 end
 
-function traceat(state::GFGenerateState, gen_fn::GenerativeFunction{T,U},
-                 args, key) where {T,U}
+@inline state_getindex(state, tr, key) = (key, ) == state.target ? Dual(getindex(tr, key), 1.0) : getindex(tr, key)
+
+function traceat(state::GFForwardModeState, 
+                 dist::Distribution{T},
+                 args, 
+                 key)
+
+    # check that key was not already visited, and mark it as visited
+    visit!(state.visitor, key)
+
+    # check for constraints at this key - if key == target, this is a dual number.
+    retval = state_getindex(state, tr, key)
+
+    # compute logpdf
+    score = logpdf(dist, retval, args...)
+
+    # increment weight
+    state.weight += score
+
+    retval
+end
+
+function traceat(state::GFForwardModeState, 
+                 gen_fn::GenerativeFunction{T,U},
+                 args, 
+                 key) where {T,U}
 
     local subtrace::U
-    local retval::T
 
     # check key was not already visited, and mark it as visited
     visit!(state.visitor, key)
@@ -27,7 +58,7 @@ function traceat(state::GFGenerateState, gen_fn::GenerativeFunction{T,U},
     # check for constraints at this key
     constraints = get_submap(state.constraints, key)
 
-    # get subtrace and weight, weight will be a dual if addr matches target.
+    # get subtrace and weight, weight will be a dual if key matches target.
     (subtrace, weight) = generate(gen_fn, args, constraints)
 
     # update weight
@@ -38,7 +69,8 @@ function traceat(state::GFGenerateState, gen_fn::GenerativeFunction{T,U},
     retval
 end
 
-function splice(state::GFGenerateState, gen_fn::DynamicDSLFunction,
+function splice(state::GFForwardModeState, 
+                gen_fn::DynamicDSLFunction,
                 args::Tuple)
     prev_params = state.params
     state.params = gen_fn.params
@@ -47,8 +79,14 @@ function splice(state::GFGenerateState, gen_fn::DynamicDSLFunction,
     retval
 end
 
-function forward(addr, params, cl, seed)
-    ctx = ForwardMode(addr, params, cl, seed)
-    ret = ctx(cl.fn, cl.args...)
-    ret, ctx.weight
+function forward(target, params, tr, seed)
+    state = GFForwardMode(target, params, cl, seed)
+    retval = exec(gen_fn, state, args)
+    (state.trace, state.weight)
+end
+
+function forward(target, tr, seed)
+    state = GFForwardMode(target, params, tr, seed)
+    retval = exec(gen_fn, state, args)
+    (state.trace, state.weight)
 end
